8d87eeb71893ae6890e423895de87b94e9d73eaf
Joss Bleach <jossbleach@MacBook-Pro.local>
2020-06-07 18:45:27 +0100

    initial commit

--- finding-lanes.py
+++ finding-lanes.py
@@ -7,8 +7,7 @@ import cv2
 
 
 def greyscale(image):
-    greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return greyscale
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 
 def darken(image, gamma=1.0):
@@ -22,14 +21,19 @@ def gaussian_blur(image, kernel_size):
     return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
 
 
+def canny(image, low_threshold, high_threshold):
+    return cv2.Canny(image, low_threshold, high_threshold)
+
+
 # Process
 image = mpimg.imread('test_images/solidWhiteRight.jpg')
 lane_image = image
 
 greyscale_image = greyscale(lane_image)
-darkened_image = darken(greyscale_image, 0.4)
-blur_image = gaussian_blur(darkened_image, 5)
+darken_image = darken(greyscale_image, 0.4)
+blur_image = gaussian_blur(darken_image, 7)
+canny_image = canny(blur_image, 50, 150)
 
 
-plt.imshow(blur_image, cmap="gray")
+plt.imshow(canny_image, cmap="gray")
 plt.show()
